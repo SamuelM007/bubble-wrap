e7b193a054e72d5d3ad6e0b3f86508fcde327f97
SamuelM007 <174657033+SamuelM007@users.noreply.github.com>
2026-01-15 08:50:16 +0000

    played around a bit and made left trigger display red, right displat green, and d pad up display blue. mix and match to see how light mixes on you computer screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,45 @@
 #include <bn_backdrop.h>
 #include <bn_color.h>
 #include <bn_core.h>
-
-
+#include <bn_keypad.h>
 
 
 int main() {
 
     bn::core::init();
 
+    bn::backdrop::set_color(bn::color(0, 0, 0));
     
-    bn::backdrop::set_color(bn::color(20, 20, 31));
-
+    
+    
     while(true) {
+        
+        int red = 0, green = 0, blue = 0;
+        if (bn::keypad::l_held()) {
+            red = 31;
+            bn::backdrop::set_color(bn::color(red, green, blue));
+        } else {
+            red = 0;
+            bn::backdrop::set_color(bn::color(red, green, blue));
+        }
+
+        if (bn::keypad::r_held()) {
+            green = 31;
+            bn::backdrop::set_color(bn::color(red, green, blue));
+        }else {
+            green = 0;
+            bn::backdrop::set_color(bn::color(red, green, blue));
+        }
+
+        if (bn::keypad::up_held()) {
+            blue = 31;
+            bn::backdrop::set_color(bn::color(red, green, blue));
+        }else {
+            blue = 0;
+            bn::backdrop::set_color(bn::color(red, green, blue));
+        }
+
+
         bn::core::update();
     }
 
